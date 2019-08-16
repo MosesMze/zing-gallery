edc660b470a98d7e6a699cfa5f7c20af03ab54e3
mengze <1194789138@qq.com>
2019-08-16 18:35:47 +0800

    修改一些功能

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	cdnDomain: '/',
-	title: 'Zing Gallery',
-	avatar: './img/avatar.png',
+	title: 'Moses Mze',
+	avatar: './img/avatar.jpg',
+	favicon: './img/Monkey.ico',
 	wording: {
 		noAccess: '抱歉，你没有权限访问'
 	},
@@ -20,6 +21,11 @@ module.exports = {
 		  name: "私密",
 		  password: "233",
 			passwordTips: "密码是233"
+		},
+		"烟柳万丝": {
+		  description : "情似游丝，人如飞絮。泪珠阁定空相觑。一溪烟柳万丝垂，无因系得兰舟住。雁过斜阳，草迷烟渚。如今已是愁无数。明朝且做莫思量，如何过得今宵去。",
+			thumbnail: "夕照.jpg",
+		  name: "踏莎行·情似游丝"
 		}
 	}
 }
